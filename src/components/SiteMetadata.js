f099def058c4a40fc2257d0aeead5385d48bff24
Herve07h22 <contact@camilab.co>
2023-01-12 12:37:40 +0100

    added new fairlytics

--- src/components/SiteMetadata.js
+++ src/components/SiteMetadata.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import { graphql, useStaticQuery } from "gatsby"
+import React from "react";
+import { Helmet } from "react-helmet";
+import { graphql, useStaticQuery } from "gatsby";
 
 const SiteMetadata = () => {
   const {
@@ -18,33 +18,37 @@ const SiteMetadata = () => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <Helmet defer={false} defaultTitle={title} titleTemplate={`%s | ${title}`}>
-        <html lang="fr" />
-        <link rel="canonical" href={`${siteUrl}`} />
-        <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
-        />
-        <meta content={description} name="description"></meta>
-        <meta property="og:url" content={siteUrl} />
-        <meta property="og:title" content={title} />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="fr" />
-        <meta property="og:site_name" content={title} />
-        <meta property="og:image" content={`${siteUrl}/${image}`} />
-        <meta property="og:image:width" content="500" />
-        <meta property="og:image:height" content="350" />
-        <meta property="og:description" content={description} />
+      <html lang="fr" />
+      <link rel="canonical" href={`${siteUrl}`} />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
+      />
+      <meta content={description} name="description"></meta>
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="fr" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:image" content={`${siteUrl}/${image}`} />
+      <meta property="og:image:width" content="500" />
+      <meta property="og:image:height" content="350" />
+      <meta property="og:description" content={description} />
 
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={title} />
-        <script defer id="fairlytics-id-ajcu6jd9k7ysd6" fairlyticskey="0fdc21bc-1494-46b6-ae4b-cf535064fce4" src="https://fairlytics.netlify.com/dist/js/tag.js"></script>
-
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      <script
+        defer
+        id="fairlytics-id-ajcu6jd9k7ysd6"
+        fairlyticskey="5d3ebce9f116a5b68efab2f0d4d69a56"
+        src="https://9writers.com/tag/tag.js"
+      ></script>
     </Helmet>
-  )
-}
+  );
+};
 
-export default SiteMetadata
+export default SiteMetadata;
